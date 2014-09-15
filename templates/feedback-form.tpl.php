8189cc38674730f0a50bf8fb2e4eca554f164665
Igor Zinovyev <zinigor@gmail.com>
2014-09-15 14:43:26 +0000

    Overhauled the feedback form styles using the WordPress styles.

--- templates/feedback-form.tpl.php
+++ templates/feedback-form.tpl.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * Template file for the Draft Feedback Form
- *
- * @todo style this (and such)
  */
 ?>
 <style>
@@ -14,16 +12,16 @@
 </script>
 
 <div class="draftfeedback-container draftfeedback-feedback-pulldown">
-	<h6><?php the_author(); ?> <?php _e( 'would like your feedback.' ); ?></h6>
-	<input type="button" id="draftfeedback-activate" value="<?php _e( 'I am ready!' ); ?>" />
+	<h3><?php the_author(); ?> <?php _e( 'would like your feedback.' ); ?></h3>
+	<input type="button" class="button" id="draftfeedback-activate" value="<?php _e( 'I am ready!' ); ?>" />
 </div>
 <div class="draftfeedback-container draftfeedback-feedback-form">
 
 	<div class="draftfeedback-thanks draftfeedback-second-screen" id="draftfeedback-thanks">
 		<h3><?php _e( 'Thank you for your feedback!' ); ?></h3>
 		<p><?php _e( "Feel free to close this page and we'll email you when the draft is published for everyone to see. If you want to send anything else, press the button below." ); ?></p>
-		<input type="button" id="feedback-more"  value="<?php _e( 'Send More Feedback' ); ?>" />
-		<input type="button" class="draftfeedback-deactivate" value="<?php _e( 'Back to post' ); ?>" />
+		<input type="button" class="button button-primary" id="feedback-more"  value="<?php _e( 'Send More Feedback' ); ?>" />
+		<input type="button" class="button draftfeedback-deactivate" value="<?php _e( 'Back to post' ); ?>" />
 	</div>
 	<div class="draftfeedback-intro draftfeedback-first-screen" id="draftfeedback-intro">
 		<h3><?php the_author(); ?> <?php _e( 'would like your feedback.' ); ?></h3>
@@ -34,7 +32,7 @@
 	</div>
 	<form class="draftfeedback-first-screen" id="feedbackform" method="post">
 		<textarea name="feedback" rows="4" id="feedback-text"></textarea>
-		<input type="submit" name="Send Feedback" value="<?php _e( 'Send Feedback' ); ?>" />
-		<input type="button" class="draftfeedback-deactivate" value="<?php _e( 'Back to post' ); ?>" />
+		<input type="submit" class="button button-primary" name="Send Feedback" value="<?php _e( 'Send Feedback' ); ?>" />
+		<input type="button" class="button draftfeedback-deactivate" value="<?php _e( 'Back to post' ); ?>" />
 	</form>
 </div>
